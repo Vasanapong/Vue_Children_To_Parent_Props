2ff4a4f644c2f3ed45cf7069f30541676ef3065d
Vasanapong <vasanapong.c@gmail.com>
2022-02-14 00:36:21 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Card :initValue=initValue @cardValueChange="cardValueChange"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Card,
+  },
+  data() {
+    return {
+      initValue: 10,
+    };
+  },
+  methods:{
+    cardValueChange(value){
+      this.initValue = value
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0px;
+  padding: 0px;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">{{initValue}}</div>
+  <button @click="cardValueChange()">Change Value</button>
+</template>
+
+<script>
+export default {
+    name:"Card",
+    props:{
+        initValue:Number
+    },
+    methods:{
+        cardValueChange(){
+            this.$emit('cardValueChange',this.initValue+10)
+        }
+    },
+    emits:["cardValueChange"]
+}
+</script>
+
+<style>
+
+</style>
